0ffb2e276c72d389f5cd639a3e1d33e4653f26da
Domagoj <domagojkuvezdic@gmail.com>
2021-05-15 12:24:00 +0200

    Creating header

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="App">
+        <Header/>
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue"
+export default{
+    components:{Header}
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="header">
+        <h1>Hello</h1>
+    </div>
+</template>
+
+<style>
+    .header{
+        width: 100%;
+        height: 100%;
+    }
+</style>
+
+<script>
+export default{
+    name:"Header"
+}
+</script>
