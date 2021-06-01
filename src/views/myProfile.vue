31188d41acf23ed632c7e0cde3f45f60757ef6a5
Domagoj <domagojkuvezdic@gmail.com>
2021-06-01 21:41:39 +0200

    Update myProfile

--- src/views/myProfile.vue
+++ src/views/myProfile.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="my__profile">
     <h1>This is my profile</h1>
+
+    <div class="profileCard">
+      <img class="profilePicture" src="../assets/user.png">
+      <div class="ime">
+        Ime
+      </div>
+      <div class="prezime">
+        Prezime
+      </div>
+      <div class="email">
+        E-mail
+      </div>
+    </div>
+    <p>Uredi osobne podatke</p>
   </div>
 </template>
 
