98a46a24ea4ee326fa1a7665b3b14807fe61e652
Adis Hadzic <adis7hadzic@gmail.com>
2021-08-31 14:09:20 +0200

    Update Base url

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import $router from '@/router';
 
 let Service = axios.create({
-  baseURL: 'https://scripty-backend.vercel.app',
+  baseURL: 'https://scripty-backend.herokuapp.com',
   timeout: 2000,
   headers: {
     Accept: 'application/json',
